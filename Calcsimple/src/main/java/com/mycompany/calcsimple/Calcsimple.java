1d3f453ea3fa182d8e658f5681a4cbb15e8ca87e
jmunozc023 <67330305+jmunozc023@users.noreply.github.com>
2022-08-02 11:22:03 -0600

    Update GUI

--- Calcsimple/src/main/java/com/mycompany/calcsimple/Calcsimple.java
+++ Calcsimple/src/main/java/com/mycompany/calcsimple/Calcsimple.java
@@ -1,17 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.calcsimple;
-
-/**
- *
- * @author josep
- */
-public class Calcsimple {
-
+import java.awt.Font;
+import javax.swing.*;
+public class Calcsimple extends JFrame{
+    private JLabel label1, label2;
+    public Calcsimple(){
+        setLayout(null);
+        label1=new JLabel("Sistema de calculadora",SwingConstants.CENTER);
+        label1.setFont(new Font("Sans",Font.BOLD,35));
+        label1.setBounds(3,30,950,30);
+        add(label1);
+        label2=new JLabel("Ingrese los dos numeros a calcular",SwingConstants.CENTER);
+        label2.setFont(new Font("Sans",Font.PLAIN,20));
+        label2.setBounds(20,100,400,60);
+        add(label2);
+        
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Calcsimple form= new Calcsimple();
+        form.setBounds(171, 134, 1024, 800);
+        form.setVisible(true);
     }
 }
